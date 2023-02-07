e93519b2c8c8c2221aeffd4fcece87b753eabd34
hollylambertt <holly.lambchops@gmail.com>
2023-02-06 20:20:56 -0700

    Mission05Finished

---
+++ Controllers/blahController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test1.Models;
+
+namespace Test1.Controllers
+{
+    public class blahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Grade()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Grade(GradeModel model)
+        {
+            return View();
+        }
+
+    }
+}
